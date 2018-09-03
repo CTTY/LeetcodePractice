7c11c6c332b1de65e1e3a0fea52062de1c68f758
CTTY <>
2018-09-03 14:11:28 -0400

    notations

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -5,38 +5,35 @@ class Solution:
         :type x: int
         :rtype: int
         """
-        bar=2**31
+        bar=2**31                      #signed 32-bit bar
         def reverse_sub(x):
             strs=str(x)
-            tempstr=""
-            for i in range(len(strs))[::-1]:
+            tempstr=""                  
+            for i in range(len(strs))[::-1]:    
                 for j in range(len(strs))[::-1]:
-                    if(strs[j]=='0'):
+                    if(strs[j]=='0'):       #deal with 0s at the end of strs
                         continue
                     else:
                         break
-                tempstr+=strs[i]
+                tempstr+=strs[i]      #to add element into string
     
             return (int(tempstr))
 
-        if x<-bar or x>bar-1:
+        if x<-bar or x>bar-1:   # #signed 32-bit input
             return 0
         if x==0:
             return 0
-        if x<0:
+        if x<0:            #deal with negative integer
             x=(-x)
             ans=0-reverse_sub(x)
         else:
             ans=reverse_sub(x)
             
-        if ans<-bar or ans>bar-1:
+        if ans<-bar or ans>bar-1:        #signed 32-bit output
             return 0
             
         return(ans)
 
-a=Solution()
-x=9
-print(a.reverse(x))
 
 
 
